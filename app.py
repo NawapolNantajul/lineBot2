e70a470d7be8825a86165cafc493dd5a786d836e
NawapolNantajul <51728695+NawapolNantajul@users.noreply.github.com>
2019-07-03 08:50:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     a=os.environ['Authorization']
-    return "นายนวพล นันทจุลเลขที่ 12 ชั้น ม.4/8"
+    return "นายนวพล นันทจุล เลขที่ 12 ชั้น ม.4/8"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
